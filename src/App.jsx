08a595bc5decd7f012e859d1e0374ed7a47f2f3e
alvaro-j <alvarojaime360@gmail.com>
2022-01-27 10:56:52 -0300

    import components and use them

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,21 @@
 import React from "react";
+import Footer from "./components/Footer";
+import FormInput from "./components/FormInput";
+import List from "./components/List";
+import { DataProvider } from "./components/DataProvider";
 
 const App = () => {
 	return (
-		<div className="main">
-			<h1>buy<span>list</span></h1>
-			<form autoComplete="off">
-				<input type="text" name="todos" id="todos" required placeholder="What needs to be done today Adonis?"></input>
-				<button type="submit">Add</button>
-			</form>
-
-			<ul>
-				<li>
-					<label htmlFor="">
-						<input type="checkbox" id="" />
-						Chicken breast
-					</label>
-					<button className="btn-yellow">Edit</button>
-				</li>
-				<li>
-					<label htmlFor="">
-						<input type="checkbox" id="" />
-						Coconut oil
-					</label>
-					<button className="btn-yellow">Edit</button>
-				</li>
-				<li>
-					<label htmlFor="">
-						<input type="checkbox" id="" />
-						Protein powder
-					</label>
-					<button className="btn-yellow">Edit</button>
-				</li>
-			</ul>
-			<div className="row">
-				<label htmlFor="all">
-					<input type="checkbox" name="all" id="all" />
-					ALL
-				</label>
-				<p>You have 0 itens to buy</p>
-				<button id="delete">Delete</button>
+		<DataProvider>
+			<div className="main">
+				<h1>
+					buy<span>list</span>
+				</h1>
+				<FormInput />
+				<List />
+				<Footer />
 			</div>
-		</div>
+		</DataProvider>
 	);
 };
 
