f2e8ce9d0745e116b63799d20cb15e125bab97e3
alvaro-j <alvarojaime360@gmail.com>
2022-01-28 10:23:58 -0300

    change todos to itens

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FormInput = () => {
 	return (
 		<form autoComplete="off">
-			<input type="text" name="todos" id="todos" required placeholder="Which items would you like to buy?"></input>
+			<input type="text" name="itens" id="itens" required placeholder="Which items would you like to buy?"></input>
 			<button type="submit">Add</button>
 		</form>
 	);
