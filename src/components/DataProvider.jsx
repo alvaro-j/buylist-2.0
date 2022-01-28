04b5f31e65fed7f833e690ece6daeeb1f114ebb9
alvaro-j <alvarojaime360@gmail.com>
2022-01-28 10:59:24 -0300

    delete test code and fix items misspell

--- src/components/DataProvider.jsx
+++ src/components/DataProvider.jsx
@@ -3,12 +3,10 @@ import React from "react";
 export const DataContext = React.createContext();
 
 export const DataProvider = ({ children }) => {
-	const [itens, setItens] = React.useState([
-		{ name: "chicken breast", complete: false },
-		{ name: "coconut oil", complete: false },
-		{ name: "brown rice", complete: true },
-		{ name: "sweet potato", complete: true },
-	]);
+	const [items, setItems] = React.useState([]);
 
-	return <DataContext.Provider value={[itens, setItens]}>{children}</DataContext.Provider>;
+
+	
+
+	return <DataContext.Provider value={[items, setItems]}>{children}</DataContext.Provider>;
 };
