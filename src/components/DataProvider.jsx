623ec48fc0548431fcecb8a3816e7a3a909ef6bc
alvaro-j <alvarojaime360@gmail.com>
2022-01-28 10:21:06 -0300

    change todos to itens

--- src/components/DataProvider.jsx
+++ src/components/DataProvider.jsx
@@ -3,11 +3,12 @@ import React from "react";
 export const DataContext = React.createContext();
 
 export const DataProvider = ({ children }) => {
-	const [todos, setTodos] = React.useState([
+	const [itens, setItens] = React.useState([
 		{ name: "chicken breast", complete: false },
 		{ name: "coconut oil", complete: false },
 		{ name: "brown rice", complete: true },
+		{ name: "sweet potato", complete: true },
 	]);
 
-	return <DataContext.Provider value={[todos, setTodos]}>{children}</DataContext.Provider>;
+	return <DataContext.Provider value={[itens, setItens]}>{children}</DataContext.Provider>;
 };
