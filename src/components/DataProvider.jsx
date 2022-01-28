fb6ac4f940e9cb28bf6f0ecd96408f772a49177a
alvaro-j <alvarojaime360@gmail.com>
2022-01-28 11:03:48 -0300

    set to localStorage

--- src/components/DataProvider.jsx
+++ src/components/DataProvider.jsx
@@ -5,8 +5,9 @@ export const DataContext = React.createContext();
 export const DataProvider = ({ children }) => {
 	const [items, setItems] = React.useState([]);
 
-
-	
+	React.useEffect(() => {
+		localStorage.setItem("itemLocal", JSON.stringify(items))
+	}, [items]);
 
 	return <DataContext.Provider value={[items, setItems]}>{children}</DataContext.Provider>;
 };
