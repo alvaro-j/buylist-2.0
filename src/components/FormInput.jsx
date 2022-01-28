819c1fcea35c010fba9e51712a3b9d797d75efbb
alvaro-j <alvarojaime360@gmail.com>
2022-01-28 10:58:47 -0300

    add useState to add item

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { DataContext } from "./DataProvider";
 
 const FormInput = () => {
+	const [items, setItems] = React.useContext(DataContext);
+	const [itemName, setItemName] = React.useState("");
+
+	const addItem = (e) => {
+		e.preventDefault();
+		setItems([...items, {name: itemName, complete: false }])
+		setItemName("");
+	};
 	return (
-		<form autoComplete="off">
-			<input type="text" name="itens" id="itens" required placeholder="Which items would you like to buy?"></input>
+		<form autoComplete="off" onSubmit={addItem}>
+			<input type="text" name="items" id="items" required placeholder="Which items would you like to buy?" value={itemName} onChange={(e) => setItemName(e.target.value.toLocaleLowerCase())}></input>
 			<button type="submit">Add</button>
 		</form>
 	);
