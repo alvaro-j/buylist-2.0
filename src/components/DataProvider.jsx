6686d7a7fd9667ed9c1016b6e630cfe35bbb3826
alvaro-j <alvarojaime360@gmail.com>
2022-02-02 10:30:34 -0300

    add comments

--- src/components/DataProvider.jsx
+++ src/components/DataProvider.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const DataContext = React.createContext();
+export const DataContext = React.createContext(); // this simple creates a context
 
-export const DataProvider = ({ children }) => {
+export const DataProvider = ({ children }) => { // this is used to hold the information
 	const [items, setItems] = React.useState([]);
 	React.useEffect(() => {
 		const itemLocal = JSON.parse(localStorage.getItem("itemLocal"));
@@ -14,4 +14,5 @@ export const DataProvider = ({ children }) => {
 	}, [items]);
 
 	return <DataContext.Provider value={[items, setItems]}>{children}</DataContext.Provider>;
+	//this 👆 is to pass down the information from the DataProvider to the children(components)
 };
