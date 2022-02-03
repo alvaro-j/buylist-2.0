68c271ccf24db011fcf26b4190fe3f093c5444db
alvaro-j <alvarojaime360@gmail.com>
2022-02-03 10:20:03 -0300

    change to input on edit button click

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 
 const ListItem = ({ item, id, checkComplete }) => {
-	const [edit, setEdit] = React.useState(false); 
-	const handleEdit = () => {};
-	return (
-		<li>
-			<label htmlFor={id} className={item.complete ? "active" : ""}>
-				<input type="checkbox" id={id} checked={item.complete} onChange={() => checkComplete(id)} />
-				{item.name}
-			</label>
-			<button className="btn-yellow" disabled={item.complete} onClick={handleEdit}>
-				Edit
-			</button>
-		</li>
-	);
+	const [edit, setEdit] = React.useState(false);
+	const handleEdit = () => {
+		setEdit(true);
+	};
+
+	if (edit) {
+		return (
+			<li>
+				<input type="text" id={id} checked={item.complete} onChange={() => checkComplete(id)} />
+
+				<button className="btn-yellow" disabled={item.complete} onClick={handleEdit}>
+					Edit
+				</button>
+			</li>
+		);
+	} else {
+		return (
+			<li>
+				<label htmlFor={id} className={item.complete ? "active" : ""}>
+					<input type="checkbox" id={id} checked={item.complete} onChange={() => checkComplete(id)} />
+					{item.name}
+				</label>
+				<button className="btn-yellow" disabled={item.complete} onClick={handleEdit}>
+					Edit
+				</button>
+			</li>
+		);
+	}
 };
 
 export default ListItem;
