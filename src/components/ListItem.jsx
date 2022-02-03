298235f60d9f687ffb3b41aeea74edf56f3b7f53
Álvaro J <86482525+alvaro-j@users.noreply.github.com>
2022-02-03 10:01:04 -0300

    Update ListItem.jsx

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 const ListItem = ({ item, id, checkComplete }) => {
+	const [edit, setEdit] = React.useState(false); 
+	const handleEdit = () => {};
 	return (
 		<li>
 			<label htmlFor={id} className={item.complete ? "active" : ""}>
 				<input type="checkbox" id={id} checked={item.complete} onChange={() => checkComplete(id)} />
 				{item.name}
 			</label>
-			<button className="btn-yellow" disabled={item.complete}>Edit</button>
+			<button className="btn-yellow" disabled={item.complete} onClick={handleEdit}>
+				Edit
+			</button>
 		</li>
 	);
 };
