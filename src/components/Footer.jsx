db58914f0d78931dbbc7734e3eac6af339390cad
alvaro-j <alvarojaime360@gmail.com>
2022-02-04 10:31:57 -0300

    add h2 when there is no item to buy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,6 @@ import { DataContext } from "./DataProvider";
 
 const Footer = () => {
 	const [items, setItems] = React.useContext(DataContext);
-	const handleDelete = () => {
-		setItems([]);
-	};
 	const [checkAll, setCheckAll] = React.useState(false);
 	const handleCheckAll = () => {
 		const newItems = [...items];
@@ -16,17 +13,30 @@ const Footer = () => {
 		setItems(newItems);
 		setCheckAll(!checkAll);
 	};
+	const handleDeleteItem = () => {
+		const newItems = items.filter((item) => {
+			return item.complete === false;
+		});
+		setItems(newItems);
+		setCheckAll(false);
+	};
 	return (
-		<div className="row">
-			<label htmlFor="all">
-				<input type="checkbox" name="all" id="all" onClick={handleCheckAll} />
-				ALL
-			</label>
-			<p>You have {items.length} items to buy</p>
-			<button id="delete" onClick={handleDelete}>
-				Delete
-			</button>
-		</div>
+		<>
+			{items.length === 0 ? (
+				<h2>You have nothing to buy!</h2>
+			) : (
+				<div className="row">
+					<label htmlFor="all">
+						<input type="checkbox" name="all" id="all" onClick={handleCheckAll} checked={checkAll} />
+						ALL
+					</label>
+					<p>You have {items.length} items to buy</p>
+					<button id="delete" onClick={handleDeleteItem}>
+						Delete
+					</button>
+				</div>
+			)}
+		</>
 	);
 };
 
