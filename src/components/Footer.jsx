b2bf23d85c53485ca6662b0f1d59419e4d61ccea
alvaro-j <alvarojaime360@gmail.com>
2022-02-04 10:15:23 -0300

    check items done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,19 @@ const Footer = () => {
 	const handleDelete = () => {
 		setItems([]);
 	};
-	console.log(items);
+	const [checkAll, setCheckAll] = React.useState(false);
+	const handleCheckAll = () => {
+		const newItems = [...items];
+		newItems.forEach((item) => {
+			item.complete = !checkAll;
+		});
+		setItems(newItems);
+		setCheckAll(!checkAll);
+	};
 	return (
 		<div className="row">
 			<label htmlFor="all">
-				<input type="checkbox" name="all" id="all" />
+				<input type="checkbox" name="all" id="all" onClick={handleCheckAll} />
 				ALL
 			</label>
 			<p>You have {items.length} items to buy</p>
