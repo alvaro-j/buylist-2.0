a16b3f599e98b89eca7d78e587c3a8d887c5fae2
alvaro-j <alvarojaime360@gmail.com>
2022-02-04 09:49:08 -0300

    show itens to buy done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
+import { DataContext } from "./DataProvider";
+
 const Footer = () => {
+	const value = React.useContext(DataContext)
+	console.log(value);
 	return (
 		<div className="row">
 			<label htmlFor="all">
 				<input type="checkbox" name="all" id="all" />
 				ALL
 			</label>
-			<p>You have 0 items to buy</p>
+			<p>You have {value[0].length} items to buy</p>
 			<button id="delete">Delete</button>
 		</div>
 	);
