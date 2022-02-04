de935fc7b8d2594f44dc96a322e99a3bddb10f89
alvaro-j <alvarojaime360@gmail.com>
2022-02-04 11:07:13 -0300

    add items complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
 						<input type="checkbox" name="all" id="all" onChange={handleCheckAll} checked={checkAll} />
 						ALL
 					</label>
-					<p>You have {items.length} items to buy</p>
+					<p>You have {items.filter(item => item.complete === false).length} items to buy</p>
 					<button id="delete" onClick={handleDeleteItem}>
 						Delete
 					</button>
